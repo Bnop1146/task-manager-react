c65e51c8d3d2c4737e2e1812a2cf084e5083fd0c
Bnop1146 <79976803+Bnop1146@users.noreply.github.com>
2023-05-29 16:22:51 +0200

    Last things updated, and all styling working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header";
+import Todo from './components/Todo';
  
 function App() {
   return (
     <div className="App">
-       <Header></Header>
+       <Header />
+       <Todo />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,13 @@ function Header() {
       <div>
         <nav className="nav">
           <div className="nav-left">
-            <a className="brand" href="#">
+            <a href='/#' className="brand">
                 Task Management React
             </a>
           </div>
           <div className="nav-right">
             <div className="tabs">
-              <a href="https://wpwebinfotech.com">Github</a>
+              <a href="https://github.com/Bnop1146/task-manager-react">Github</a>
             </div>
           </div>
         </nav>
